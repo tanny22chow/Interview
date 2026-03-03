c3a53419e74bcf806a7e192d0ec71b52f61ded7f
tanmoy <tanmoy.choudhury@publicissapient.com>
2026-03-03 12:56:04 +0530

    bs added

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,11 +1,17 @@
 package org.example;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.MutableCapabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 /**
  * Unit test for simple App.
@@ -13,19 +19,22 @@ import org.testng.annotations.Test;
 public class AppTest 
 
 {
-    WebDriver d;
-    @BeforeTest
-    public void setup(){
-        WebDriverManager.chromedriver().setup();
-        d=new ChromeDriver();
+    WebDriver driver;
+    @BeforeMethod
+    public void setup() throws MalformedURLException {
+        String URL = "https://" + "ravindrand_dQv6wj" + ":" + "ZkcKwXKuxLRqskearE35" + "@hub.browserstack.com/wd/hub";
+        MutableCapabilities capabilities = new MutableCapabilities();
+        driver = new RemoteWebDriver(new URL(URL), capabilities);
+
     }
     @Test
     public void m1(){
-        d.navigate().to("https://medium.com/");
+        driver.navigate().to("https://medium.com/");
+        System.out.println(driver.getPageSource());
 
     }
     @AfterTest
     public void destroy(){
-        d.quit();
+        driver.quit();
     }
 }
